a474f1c9cd2261d42ff8cbce2732c5f8907f87a7
Richard Passos <richard.ac.passos12@gmail.com>
2024-07-31 17:09:32 -0300

    REFA remove unn code

--- src/store/root.ts
+++ src/store/root.ts
@@ -1,4 +1,4 @@
-import { Slice, combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 

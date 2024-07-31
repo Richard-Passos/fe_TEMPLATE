0d77f1059cee8e2b877cab2a93948c71cfe7262a
Richard Passos <richard.ac.passos12@gmail.com>
2024-07-31 17:31:56 -0300

    CHORE export

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,28 +1,36 @@
 import Badge from './Badge';
+import Bg from './Bg';
 import Button from './Button';
 import Divider from './Divider';
 import Icon from './Icon';
 import Image from './Image';
 import Indicator from './Indicator';
+import Lines from './Lines';
 import Link from './Link';
 import Loader from './Loader';
 import Magnetic from './Magnetic';
 import Marquee from './Marquee';
+import Portal from './Portal';
+import ScrollAnimate from './ScrollAnimate';
 import Select from './Select';
 import Text from './Text';
 import Title from './Title';
 
 const Atoms = {
   Badge,
+  Bg,
   Button,
   Divider,
   Icon,
   Image,
   Indicator,
+  Lines,
   Link,
   Loader,
   Magnetic,
   Marquee,
+  Portal,
+  ScrollAnimate,
   Select,
   Text,
   Title
@@ -31,15 +39,19 @@ const Atoms = {
 export default Atoms;
 export {
   Badge,
+  Bg,
   Button,
   Divider,
   Icon,
   Image,
   Indicator,
+  Lines,
   Link,
   Loader,
   Magnetic,
   Marquee,
+  Portal,
+  ScrollAnimate,
   Select,
   Text,
   Title

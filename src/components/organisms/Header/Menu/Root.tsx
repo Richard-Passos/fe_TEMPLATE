6ed8ff5cc794442fd7b6f16a17307cb756874336
Richard Passos <richard.ac.passos12@gmail.com>
2024-07-31 17:27:47 -0300

    CHORE add content

--- src/components/organisms/Header/Menu/Root.tsx
+++ src/components/organisms/Header/Menu/Root.tsx
@@ -1,23 +1,67 @@
-import { ComponentPropsWithRef, ForwardedRef, forwardRef } from 'react';
+import { useTranslations } from 'next-intl';
+import { forwardRef } from 'react';
 
+import { Portal } from '@/components/atoms';
+import { Drawer } from '@/components/molecules';
+import {
+  DrawerContentProps,
+  DrawerRootProps,
+  DrawerTriggerProps
+} from '@/components/molecules/Drawer';
 import { cn } from '@/utils';
 
-type HeaderMenuOrganismProps = ComponentPropsWithRef<'div'>;
+import HeaderMenuTrigger from './Trigger';
 
-type HeaderMenuOrganismRef = ForwardedRef<HTMLDivElement>;
+type HeaderMenuOrganismOwnProps = Partial<Pick<DrawerRootProps, 'trigger'>> & {
+  namespace: ExtractPrefix<Namespace, `${string}.menu`>;
+  triggerProps?: DrawerTriggerProps;
+  contentProps?: DrawerContentProps;
+};
+
+type HeaderMenuOrganismProps = HeaderMenuOrganismOwnProps &
+  Omit<DrawerRootProps, keyof HeaderMenuOrganismOwnProps>;
 
 const HeaderMenuOrganism = (
-  { className, ...props }: HeaderMenuOrganismProps,
-  ref: HeaderMenuOrganismRef
+  {
+    namespace,
+    className,
+    triggerProps,
+    contentProps,
+    ...props
+  }: HeaderMenuOrganismProps,
+  ref: HeaderMenuOrganismProps['ref']
 ) => {
+  const t = useTranslations(namespace);
+
   return (
-    <div
-      className={cn('', className)}
+    <Drawer.Root
+      className={cn('*:left-0 md:hidden', className)}
       ref={ref}
       {...props}
-    />
+      trigger={
+        <Portal>
+          <HeaderMenuTrigger
+            aria-label={t('open.label')}
+            className='fixed right-[2.5%] top-[calc(var(--header-h)/2)] z-[60] -translate-y-1/2'
+          />
+        </Portal>
+      }
+    >
+      <Drawer.Content
+        {...contentProps}
+        closeProps={{
+          'aria-label': t('close.label'),
+          ...contentProps?.closeProps
+        }}
+        overlayProps={{
+          className: cn('md:hidden', contentProps?.overlayProps?.className),
+          ...contentProps?.overlayProps
+        }}
+        title={t('title')}
+      ></Drawer.Content>
+    </Drawer.Root>
   );
 };
 
 export default forwardRef(HeaderMenuOrganism);
-export type { HeaderMenuOrganismProps, HeaderMenuOrganismRef };
+export type { HeaderMenuOrganismProps };

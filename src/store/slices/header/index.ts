bb363064a18f4e9bb067f7d2bed85145cd9cebf3
Richard Passos <richard.ac.passos12@gmail.com>
2024-07-31 17:04:55 -0300

    FEAT create new store slices

---
+++ src/store/slices/header/index.ts
@@ -0,0 +1,9 @@
+import HeaderSlice, {
+  HeaderSliceInitialState,
+  setHeight,
+  setIsVisible
+} from './root';
+
+export default HeaderSlice;
+export { setHeight, setIsVisible };
+export type { HeaderSliceInitialState };

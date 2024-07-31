851b39e6b305c72fe0a2a979bc5f4039cf87449f
Richard Passos <richard.ac.passos12@gmail.com>
2024-07-31 17:09:43 -0300

    CHORE export

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,12 +1,37 @@
+import useColorScheme from './useColorScheme';
+import useComputedColorScheme from './useComputedColorScheme';
+import useDisclosure from './useDisclosure';
+import useElementSize from './useElementSize';
 import useEventListener from './useEventListener';
+import useId from './useId';
 import useSmooth from './useSmooth';
 import useUpdateEffect from './useUpdateEffect';
+import useUpdateMotionValueEvent from './useUpdateMotionValueEvent';
+import useWindowSize from './useWindowSize';
 
 const hooks = {
+  useColorScheme,
+  useComputedColorScheme,
+  useDisclosure,
+  useElementSize,
   useEventListener,
+  useId,
   useSmooth,
-  useUpdateEffect
+  useUpdateEffect,
+  useUpdateMotionValueEvent,
+  useWindowSize
 };
 
 export default hooks;
-export { useEventListener, useSmooth, useUpdateEffect };
+export {
+  useColorScheme,
+  useComputedColorScheme,
+  useDisclosure,
+  useElementSize,
+  useEventListener,
+  useId,
+  useSmooth,
+  useUpdateEffect,
+  useUpdateMotionValueEvent,
+  useWindowSize
+};

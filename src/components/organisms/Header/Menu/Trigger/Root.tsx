68f91fc06c0512f25d033ccc6f8a0ef5ea3875a5
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-01 07:24:04 -0300

    CHORE test desing

--- src/components/organisms/Header/Menu/Trigger/Root.tsx
+++ src/components/organisms/Header/Menu/Trigger/Root.tsx
@@ -5,7 +5,6 @@ import { MenuIcon, TimesIcon } from '@/components/atoms/Icon/icons';
 import { ConfigOptions } from '@/components/atoms/ScrollAnimate';
 import { HeaderState } from '@/components/organisms/Header';
 import { smoothConfig } from '@/hooks/useSmooth';
-import { cn } from '@/utils';
 
 import HeaderMenuTriggerButton, {
   HeaderMenuTriggerButtonProps
@@ -22,7 +21,7 @@ const HeaderMenuTriggerOrganism = (
 ) => {
   const animationConfig: ConfigOptions = {
     scroll: 'scrollY',
-    scrollPoints: [0, 199.999, 200],
+    scrollPoints: [0, 99.999, 100],
     prop: '--tw-scale-x',
     propPoints: [0, 0, 1]
   };
@@ -34,18 +33,18 @@ const HeaderMenuTriggerOrganism = (
           config={animationConfig}
           smoothConfig={smoothConfig}
         >
-          <div className='fixed right-[calc(2.5%+var(--removed-body-scroll-bar-size,0px))] top-[calc(var(--header-h)/2)] z-[60] -translate-y-1/2 [--tw-scale-y:--tw-scale-x] data-open:![--tw-scale-x:1]'>
+          <div className='fixed right-[calc(var(--side)+var(--removed-body-scroll-bar-size,0px))] top-[--side] z-[60] flex items-center justify-center [--side:--spacing-xl] [--tw-scale-y:--tw-scale-x] data-open:![--tw-scale-x:1]'>
             <HeaderMenuTriggerButton
-              className={cn('relative', className)}
               isIconOnly
+              radius='100%'
               ref={ref}
-              size='xl'
+              size={'80'}
               variant='default'
               {...props}
             >
-              <MenuIcon className='absolute inset-1/4 data-open:hidden' />
+              <MenuIcon className='absolute inset-1/4 group-data-open/action:hidden' />
 
-              <TimesIcon className='absolute inset-1/4 data-closed:hidden' />
+              <TimesIcon className='absolute inset-1/4 group-data-closed/action:hidden' />
             </HeaderMenuTriggerButton>
           </div>
         </ScrollAnimate>

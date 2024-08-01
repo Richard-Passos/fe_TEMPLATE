29c03eb6534fc565490666fc67cc64d89a37289e
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-01 05:05:22 -0300

    FEAT create SmoothScroll atom

---
+++ src/components/atoms/SmoothScroll/Root.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { ReactLenis } from '@studio-freight/react-lenis';
+import { ComponentPropsWithRef, forwardRef } from 'react';
+
+type SmoothScrollAtomOwnProps = {};
+
+type SmoothScrollAtomProps = SmoothScrollAtomOwnProps &
+  Omit<
+    ComponentPropsWithRef<typeof ReactLenis>,
+    keyof SmoothScrollAtomOwnProps
+  >;
+
+const SmoothScrollAtom = (
+  { className, ...props }: SmoothScrollAtomProps,
+  ref: SmoothScrollAtomProps['ref']
+) => {
+  return (
+    <ReactLenis
+      ref={ref}
+      root
+      {...props}
+    />
+  );
+};
+
+export default forwardRef(SmoothScrollAtom);
+export type { SmoothScrollAtomProps };
---
+++ src/components/atoms/SmoothScroll/index.ts
@@ -0,0 +1,6 @@
+import SmoothScroll, {
+  type SmoothScrollAtomProps as SmoothScrollProps
+} from './Root';
+
+export default SmoothScroll;
+export type { SmoothScrollProps };

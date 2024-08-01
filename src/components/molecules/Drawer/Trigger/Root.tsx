4c3132dbc8d0ff3e08ff68fd25d372a3d5db5e73
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-01 03:10:04 -0300

    FIX poly

--- src/components/molecules/Drawer/Trigger/Root.tsx
+++ src/components/molecules/Drawer/Trigger/Root.tsx
@@ -3,13 +3,15 @@
 import { createPolymorphicComponent } from '@mantine/core';
 import { ComponentPropsWithRef, forwardRef } from 'react';
 
+import { Box } from '@/components/atoms';
+import { BoxProps } from '@/components/atoms/Box';
 import { ButtonAtomProps } from '@/components/atoms/Button/Root';
 import { useDisclosureContext } from '@/hooks/contexts';
 
-type DrawerTriggerMoleculeOwnProps = {};
+type DrawerTriggerMoleculeOwnProps = ComponentPropsWithRef<'button'>;
 
 type DrawerTriggerMoleculeProps = DrawerTriggerMoleculeOwnProps &
-  Omit<ComponentPropsWithRef<'button'>, keyof DrawerTriggerMoleculeOwnProps>;
+  Omit<BoxProps, keyof DrawerTriggerMoleculeOwnProps>;
 
 const DrawerTriggerMolecule = (
   props: DrawerTriggerMoleculeProps,
@@ -18,7 +20,8 @@ const DrawerTriggerMolecule = (
   const { dataState, toggle } = useDisclosureContext();
 
   return (
-    <button
+    <Box
+      component='button'
       data-state={dataState}
       ref={ref}
       {...props}

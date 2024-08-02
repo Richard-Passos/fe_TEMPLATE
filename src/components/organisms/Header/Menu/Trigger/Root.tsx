1dc3d6fc3859da96be0013467915b9617dcecfba
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-02 17:33:40 -0300

    CHORE style

--- src/components/organisms/Header/Menu/Trigger/Root.tsx
+++ src/components/organisms/Header/Menu/Trigger/Root.tsx
@@ -2,9 +2,10 @@ import { forwardRef } from 'react';
 
 import { Portal, ScrollAnimate } from '@/components/atoms';
 import { MenuIcon, TimesIcon } from '@/components/atoms/Icon/icons';
-import { ConfigOptions } from '@/components/atoms/ScrollAnimate';
+import { ScrollAnimateConfigOptions } from '@/components/atoms/ScrollAnimate';
 import { HeaderState } from '@/components/organisms/Header';
 import { smoothConfig } from '@/hooks/useSmooth';
+import { cn } from '@/utils';
 
 import HeaderMenuTriggerButton, {
   HeaderMenuTriggerButtonProps
@@ -19,7 +20,7 @@ const HeaderMenuTriggerOrganism = (
   { className, ...props }: HeaderMenuTriggerOrganismProps,
   ref: HeaderMenuTriggerOrganismProps['ref']
 ) => {
-  const animationConfig: ConfigOptions = {
+  const animationConfig: ScrollAnimateConfigOptions = {
     scroll: 'scrollY',
     scrollPoints: [0, 99.999, 100],
     prop: '--tw-scale-x',
@@ -33,18 +34,18 @@ const HeaderMenuTriggerOrganism = (
           config={animationConfig}
           smoothConfig={smoothConfig}
         >
-          <div className='fixed right-[calc(var(--side)+var(--removed-body-scroll-bar-size,0px))] top-[--side] z-[60] flex items-center justify-center [--side:--spacing-xl] [--tw-scale-y:--tw-scale-x] data-open:![--tw-scale-x:1]'>
+          <div className='fixed right-[calc(var(--side)+var(--removed-body-scroll-bar-size,0px))] top-[--side] z-max flex items-center justify-center [--side:--spacing-xl] [--tw-scale-y:--tw-scale-x] data-open:![--tw-scale-x:1]'>
             <HeaderMenuTriggerButton
+              className={cn('rounded-full', className)}
               isIconOnly
-              radius='100%'
               ref={ref}
-              size={'80'}
+              size={'72'}
               variant='default'
               {...props}
             >
-              <MenuIcon className='absolute inset-1/4 group-data-open/action:hidden' />
+              <MenuIcon className='absolute inset-[30%] group-data-open/action:hidden' />
 
-              <TimesIcon className='absolute inset-1/4 group-data-closed/action:hidden' />
+              <TimesIcon className='absolute inset-[30%] group-data-closed/action:hidden' />
             </HeaderMenuTriggerButton>
           </div>
         </ScrollAnimate>

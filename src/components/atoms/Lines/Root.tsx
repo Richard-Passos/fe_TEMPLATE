6c5e08ba9cfc82cb57799e4c449ffb44e5d55efb
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-02 17:32:20 -0300

    CHORE add bg-center

--- src/components/atoms/Lines/Root.tsx
+++ src/components/atoms/Lines/Root.tsx
@@ -16,7 +16,7 @@ const LinesAtom = (
   return (
     <div
       className={cn(
-        'fixed inset-y-0 left-1/2 w-[calc(100vw-var(--removed-body-scroll-bar-size,0px))] -translate-x-[(50%_+_var(--removed-body-scroll-bar-size,0px))] bg-[linear-gradient(currentColor_.8px,transparent_.8px),linear-gradient(to_right,currentColor_.8px,transparent_.8px)] opacity-20 [background-size:250px_200px] dark:opacity-5',
+        'fixed inset-y-0 left-1/2 w-[calc(100vw-var(--removed-body-scroll-bar-size,0px))] -translate-x-[(50%_+_var(--removed-body-scroll-bar-size,0px))] bg-[linear-gradient(currentColor_.8px,transparent_.8px),linear-gradient(to_right,currentColor_.8px,transparent_.8px)] bg-center opacity-20 [background-size:250px_200px] dark:opacity-5',
         className
       )}
       ref={ref}

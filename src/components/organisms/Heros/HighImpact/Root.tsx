315f6233cbb72b4ceac70e93e7f7f787ca06966f
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-05 11:32:23 -0300

    CHORE test style

--- src/components/organisms/Heros/HighImpact/Root.tsx
+++ src/components/organisms/Heros/HighImpact/Root.tsx
@@ -30,23 +30,27 @@ const HighImpactHeroOrganism = (
       ref={ref}
       {...props}
       className={cn(
-        'overflow-y-clip p-0 [--inset:calc(var(--w)*.025)] [--w:100vw] 2xl:[--w:--max-w]',
+        'min-h-svh p-[--inset] pt-[--header-h] [--inset:calc(var(--w)*.025)] [--w:100vw] 2xl:[--w:--max-w]',
         className
       )}
     >
-      <HighImpactHeroScrollAnimate>
-        <div className='relative flex min-h-[inherit] w-full flex-col items-center justify-center p-[--inset] pt-[--header-h]'>
-          <div className='flex w-full grow flex-col items-center justify-center gap-lg p-[--inset] sm:scale-[--scale] sm:opacity-[--opacity]'>
-            <HighImpactHeroTitle namespace={namespace} />
+      <div className='relative flex w-full grow overflow-hidden rounded-lg'>
+        <HighImpactHeroScrollAnimate>
+          <div className='relative flex w-full flex-col items-center justify-center'>
+            <div className='flex w-full grow flex-col items-center justify-center gap-lg p-[calc(var(--inset)*1.5)] sm:scale-[--scale] sm:opacity-[--opacity]'>
+              <HighImpactHeroTitle namespace={namespace} />
 
-            <HighImpactHeroExtra namespace={namespace} />
+              <HighImpactHeroExtra namespace={namespace} />
+            </div>
+
+            <Lines className='absolute -inset-y-full inset-x-0 -z-10 w-auto translate-x-0 [background-size:83.333px_66.666px]' />
           </div>
+        </HighImpactHeroScrollAnimate>
 
-          <Lines className='absolute inset-[--inset] top-[--header-h] -z-10 w-auto translate-x-0 rounded-lg border border-solid border-current [background-size:83.333px_66.666px]' />
-        </div>
-      </HighImpactHeroScrollAnimate>
+        <span className='pointer-events-none absolute inset-0 rounded-inherit border border-solid border-current opacity-20 dark:opacity-5' />
+      </div>
 
-      <ScrollIndicator className='absolute bottom-[calc(var(--inset)*1.5)] right-[calc(var(--inset)*1.5)]' />
+      <ScrollIndicator className='absolute bottom-[calc(var(--inset)*1.5)] right-[calc(var(--inset)*1.5)] max-sm:hidden' />
     </Section>
   );
 };

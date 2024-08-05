2d349ff7d1b227f694aac2b9ed794716da613f17
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-05 11:34:04 -0300

    FIX remove @mantine/hooks, add own useHook

--- src/hooks/useEventListener.tsx
+++ src/hooks/useEventListener.tsx
@@ -1,5 +1,22 @@
 'use client';
 
-import { useEventListener } from '@mantine/hooks';
+import { RefObject, useEffect, useRef } from 'react';
+
+const useEventListener = (
+  type: keyof HTMLElementEventMap,
+  listener: (this: HTMLElement, ev: Event) => any,
+  element?: RefObject<HTMLElement>,
+  options?: EventListenerOptions
+) => {
+  listener = useRef(listener).current;
+
+  useEffect(() => {
+    const targetEl = element?.current ?? window;
+
+    targetEl.addEventListener(type, listener, options);
+
+    return () => targetEl.removeEventListener(type, listener, options);
+  }, [element, type, listener, options]);
+};
 
 export default useEventListener;

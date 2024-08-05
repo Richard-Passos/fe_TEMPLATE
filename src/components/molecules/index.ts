f487f1d0e0d04b0fa234844c75c44710169420c4
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-05 11:30:34 -0300

    CHORE export

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -2,13 +2,15 @@ import Action from './Action';
 import Drawer from './Drawer';
 import List from './List';
 import LocaleSelect from './LocaleSelect';
+import ScrollTo from './ScrollTo';
 
 const Molecules = {
   Action,
   Drawer,
   List,
-  LocaleSelect
+  LocaleSelect,
+  ScrollTo
 };
 
 export default Molecules;
-export { Action, Drawer, List, LocaleSelect };
+export { Action, Drawer, List, LocaleSelect, ScrollTo };

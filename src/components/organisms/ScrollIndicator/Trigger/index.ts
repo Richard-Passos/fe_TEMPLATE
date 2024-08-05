48d9d709e28b575da753154423316227e526c700
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-05 11:30:10 -0300

    CHORE add ScrollTo

---
+++ src/components/organisms/ScrollIndicator/Trigger/index.ts
@@ -0,0 +1,6 @@
+import ScrollIndicatorTrigger, {
+  type ScrollIndicatorTriggerOrganismProps as ScrollIndicatorTriggerProps
+} from './Root';
+
+export default ScrollIndicatorTrigger;
+export type { ScrollIndicatorTriggerProps };

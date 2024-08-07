ec4a61f95acebd9cb4cf7bc00f40e549e1a8c8f6
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-07 19:07:29 -0300

    CHORE export

---
+++ src/components/organisms/Blocks/Layout/index.ts
@@ -0,0 +1,12 @@
+import CleanLayoutBlock from './Clean';
+import PrimaryLayoutBlock from './Primary';
+import SecondaryLayoutBlock from './Secondary';
+
+const LayoutBlocks = {
+  Clean: CleanLayoutBlock,
+  Primary: PrimaryLayoutBlock,
+  Secondary: SecondaryLayoutBlock
+};
+
+export default LayoutBlocks;
+export { CleanLayoutBlock, PrimaryLayoutBlock, SecondaryLayoutBlock };

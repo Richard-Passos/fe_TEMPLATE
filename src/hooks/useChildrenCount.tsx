367a3f2ef715ae4e6b96f9e7520b131485e80895
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-07 18:57:24 -0300

    FEAT create useChildrenCount

---
+++ src/hooks/useChildrenCount.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { RefObject, useCallback, useEffect, useState } from 'react';
+
+import useEventListener from './useEventListener';
+
+const useChildrenCount = (
+  parentRef: RefObject<HTMLElement>,
+  childrenRef: RefObject<HTMLElement>,
+  times = 2
+) => {
+  const [count, setCount] = useState(1);
+
+  const handleSetCount = useCallback(() => {
+    if (!(parentRef.current && childrenRef.current)) return;
+
+    const { parentSize, childrenSize } = {
+      parentSize: parentRef.current.offsetWidth,
+      childrenSize: childrenRef.current.offsetWidth
+    };
+
+    const num = Math.ceil((parentSize * times) / childrenSize);
+
+    setCount(Math.max(num % 2 !== 0 ? num : num - 1, 1));
+  }, [parentRef, childrenRef, times, setCount]);
+
+  useEventListener('resize', handleSetCount);
+
+  useEffect(() => {
+    handleSetCount();
+  }, [handleSetCount]);
+
+  return count === Infinity ? 1 : count;
+};
+
+export default useChildrenCount;

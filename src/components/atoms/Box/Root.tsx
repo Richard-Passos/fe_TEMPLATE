cf72c2edf79ff999138d1e4757ac5d99cb9df385
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-07 19:05:59 -0300

    CHORE add id

--- src/components/atoms/Box/Root.tsx
+++ src/components/atoms/Box/Root.tsx
@@ -1,13 +1,21 @@
 import { Box, BoxProps } from '@mantine/core';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, forwardRef } from 'react';
 
 type BoxAtomOwnProps = PropsWithChildren<{
   ref?: PolymorphicRef<'div'>;
+  id?: string;
 }>;
 
 type BoxAtomProps = BoxAtomOwnProps & Omit<BoxProps, keyof BoxAtomOwnProps>;
 
-const BoxAtom = Box;
+const BoxAtom = (props: BoxAtomProps, ref: BoxAtomProps['ref']) => {
+  return (
+    <Box
+      ref={ref}
+      {...props}
+    />
+  );
+};
 
-export default BoxAtom;
+export default forwardRef(BoxAtom);
 export type { BoxAtomProps };

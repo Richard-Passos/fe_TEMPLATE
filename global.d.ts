13f840dc2b0ad2a6447e1ea534cfe0b462a205cc
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-13 12:13:04 -0300

    CHORE move some types

--- global.d.ts
+++ global.d.ts
@@ -1,20 +1,7 @@
-import { NamespaceKeys, NestedKeyOf } from 'next-intl';
-import { ComponentPropsWithRef, ElementType } from 'react';
-
 import en from './src/data/en.json';
 
 type Messages = typeof en;
 
 declare global {
   interface IntlMessages extends Messages {}
-
-  type Namespace = NamespaceKeys<IntlMessages, NestedKeyOf<IntlMessages>>;
-
-  type PolymorphicRef<C extends ElementType> = ComponentPropsWithRef<C>['ref'];
-
-  type ExtractPrefix<T, P extends string> = T extends `${P}${infer Rest}`
-    ? Rest extends `${infer _}.${string}`
-      ? never
-      : T
-    : never;
 }

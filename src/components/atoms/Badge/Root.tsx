329ecf98250f02409ad632a13acba84319c4d5bb
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-17 19:51:57 -0300

    CHORE change style

--- src/components/atoms/Badge/Root.tsx
+++ src/components/atoms/Badge/Root.tsx
@@ -1,6 +1,8 @@
-import { Badge, BadgeProps } from '@mantine/core';
+import { Badge, BadgeProps, createPolymorphicComponent } from '@mantine/core';
+import { forwardRef } from 'react';
 
 import { PolymorphicRef } from '@/types';
+import { cn } from '@/utils';
 
 type BadgeAtomOwnProps = {
   ref?: PolymorphicRef<'div'>;
@@ -9,7 +11,23 @@ type BadgeAtomOwnProps = {
 type BadgeAtomProps = BadgeAtomOwnProps &
   Omit<BadgeProps, keyof BadgeAtomOwnProps>;
 
-const BadgeAtom = Badge;
+const BadgeAtom = (
+  { className, ...props }: BadgeAtomProps,
+  ref: BadgeAtomProps['ref']
+) => {
+  return (
+    <Badge
+      className={cn(
+        'normal-case [--badge-fz-md:calc(.75rem*var(--mantine-scale))] [--badge-height-md:calc(1.5rem*var(--mantine-scale))]',
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+};
 
-export default BadgeAtom;
+export default createPolymorphicComponent<'div', BadgeAtomProps>(
+  forwardRef(BadgeAtom)
+);
 export type { BadgeAtomProps };

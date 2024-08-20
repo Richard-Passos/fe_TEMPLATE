bfe222b99cb4e7ee76a82d03c3230ebe33b171bf
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-19 23:52:24 -0300

    CHORE make it ul, CHORE style, CHORE better children

--- src/components/molecules/Catalog/List/Root.tsx
+++ src/components/molecules/Catalog/List/Root.tsx
@@ -5,16 +5,25 @@ import { ReactNode, forwardRef } from 'react';
 
 import Box, { BoxProps } from '@/components/atoms/Box';
 import { useCatalogContext } from '@/hooks/contexts';
+import { PolymorphicRef } from '@/types';
+import { cn } from '@/utils';
+
+type CatalogListMoleculeChildren<T> = (
+  value: T,
+  index: number,
+  array: T[]
+) => ReactNode;
 
 type CatalogListMoleculeOwnProps<T> = {
-  children?: (value: T, index: number, array: T[]) => ReactNode;
+  children?: ReactNode | CatalogListMoleculeChildren<T>;
+  ref?: PolymorphicRef<'ul'> & BoxProps['ref'];
 };
 
 type CatalogListMoleculeProps<T> = CatalogListMoleculeOwnProps<T> &
   Omit<BoxProps, keyof CatalogListMoleculeOwnProps<T>>;
 
 const CatalogListMolecule = <T,>(
-  { children, ...props }: CatalogListMoleculeProps<T>,
+  { className, children, ...props }: CatalogListMoleculeProps<T>,
   ref: CatalogListMoleculeProps<T>['ref']
 ) => {
   const { items } = useCatalogContext<T>();
@@ -23,16 +32,17 @@ const CatalogListMolecule = <T,>(
 
   return (
     <Box
-      component='nav'
+      className={cn('m-0 list-none p-0', className)}
+      component='ul'
       ref={ref}
       {...props}
     >
-      {children && items.map(children)}
+      {typeof children === 'function' ? items.map(children) : children}
     </Box>
   );
 };
 
-export default createPolymorphicComponent<'nav', CatalogListMoleculeProps<any>>(
+export default createPolymorphicComponent<'ul', CatalogListMoleculeProps<any>>(
   forwardRef(CatalogListMolecule)
 );
-export type { CatalogListMoleculeProps };
+export type { CatalogListMoleculeProps, CatalogListMoleculeChildren };

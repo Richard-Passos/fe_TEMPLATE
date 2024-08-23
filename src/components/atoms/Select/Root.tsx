c28e6f2839b06e9e5985301da9ea35b4d0895ee4
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-23 19:36:53 -0300

    CHORE styke

--- src/components/atoms/Select/Root.tsx
+++ src/components/atoms/Select/Root.tsx
@@ -1,15 +1,83 @@
-import { Select, SelectProps } from '@mantine/core';
+import { Select, SelectProps, createPolymorphicComponent } from '@mantine/core';
+import { forwardRef } from 'react';
 
 import { PolymorphicRef } from '@/types';
+import { cn } from '@/utils';
+
+const CLASS_NAMES = {
+  size: {
+    xs: '',
+    sm: '',
+    md: '[&_input]:[--input-size:theme(height.12)]',
+    lg: '',
+    xl: ''
+  },
+  variant: {
+    default:
+      '[&_input]:[--input-bg:theme(colors.white)] dark:[&_input]:[--input-bg:theme(colors.black)]',
+    filled:
+      '[&_input]:[--input-bg:theme(colors.gray.1)] dark:[&_input]:[--input-bg:theme(colors.dark.5)]',
+    unstyled: ''
+  }
+};
 
 type SelectAtomOwnProps = {
   ref?: PolymorphicRef<'input'>;
+  size?: keyof (typeof CLASS_NAMES)['size'];
+  variant?: keyof (typeof CLASS_NAMES)['variant'];
 };
 
 type SelectAtomProps = SelectAtomOwnProps &
   Omit<SelectProps, keyof SelectAtomOwnProps>;
 
-const SelectAtom = Select;
+const SelectAtom = (
+  {
+    className,
+    size = 'sm',
+    variant = 'default',
+    comboboxProps,
+    labelProps,
+    ...props
+  }: SelectAtomProps,
+  ref: SelectAtomProps['ref']
+) => {
+  return (
+    <Select
+      className={cn(
+        CLASS_NAMES.size[size],
+        CLASS_NAMES.variant[variant],
+        className
+      )}
+      comboboxProps={{
+        withinPortal: false,
+        ...comboboxProps,
+        classNames: {
+          ...comboboxProps?.classNames,
+          dropdown: cn(
+            'bg-white dark:bg-dark-6 [--popover-border-color:theme(colors.border)]',
+            //@ts-ignore
+            comboboxProps?.classNames?.dropdown
+          ),
+          option: cn(
+            'hover:bg-gray-0 dark:hover:bg-dark-7',
+            //@ts-ignore
+            comboboxProps?.classNames?.option
+          )
+        }
+      }}
+      labelProps={{
+        ...labelProps,
+        className: cn('text-sm', labelProps?.className)
+      }}
+      ref={ref}
+      size={size}
+      variant={variant}
+      {...props}
+    />
+  );
+};
 
-export default SelectAtom;
+export default createPolymorphicComponent<'input', SelectAtomProps>(
+  forwardRef(SelectAtom)
+);
 export type { SelectAtomProps };

b59454689631f380a8f4f7a533635372e218a46c
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-23 19:34:21 -0300

    CHORE style

--- src/components/organisms/Heros/Secondary/Root.tsx
+++ src/components/organisms/Heros/Secondary/Root.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, forwardRef } from 'react';
 
-import { Title } from '@/components/atoms';
+import { yFullScrollAnim } from '@/animations/scroll';
+import { Lines, ScrollAnimate, Title } from '@/components/atoms';
 import Section, { SectionProps } from '@/components/organisms/Section';
 import { cn } from '@/utils';
 
@@ -20,18 +21,29 @@ const SecondaryHeroOrganism = (
   return (
     <Section
       className={cn(
-        'min-h-svh justify-center pt-[calc(var(--header-h)+var(--py))] 2xl:min-h-bounds',
+        'min-h-fit pt-[calc(var(--header-h)+var(--section-spacing-md))] 2xl:min-h-fit',
         className
       )}
+      hasTransition={false}
       ref={ref}
       {...props}
     >
-      <Title
-        className='text-center'
-        order={1}
-      >
-        {data.title}
-      </Title>
+      <div className='relative w-9/10 max-w-screen-lg pb-[calc(theme(spacing.2xl)*1.5)] pt-2xl'>
+        <Title
+          className='relative z-10 max-w-[54rem] break-all'
+          order={1}
+        >
+          {data.title}
+        </Title>
+
+        <div className='absolute inset-y-0 right-0 w-full max-w-md overflow-hidden rounded-lg bg-white dark:bg-black'>
+          <ScrollAnimate config={yFullScrollAnim}>
+            <Lines className='!opacity-60 [background-size:83.333px_66.666px]' />
+          </ScrollAnimate>
+
+          <span className='pointer-events-none absolute inset-0 rounded-inherit border opacity-60' />
+        </div>
+      </div>
     </Section>
   );
 };

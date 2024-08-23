b42af12968aca654bd58e06cc027781fafb6da7a
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-23 19:35:37 -0300

    CHORE styke

--- src/components/organisms/Blocks/Skills/Root.tsx
+++ src/components/organisms/Blocks/Skills/Root.tsx
@@ -1,6 +1,5 @@
 import { forwardRef } from 'react';
 
-import { HorizontalScroll } from '@/components/atoms';
 import SkillCard, { SkillCardProps } from '@/components/organisms/Cards/Skill';
 import { cn } from '@/utils';
 
@@ -10,7 +9,7 @@ import SecondaryLayoutBlock, {
 
 type SkillsBlockOrganismOwnProps = {
   data: SecondaryLayoutBlockProps['data'] & {
-    items: SkillCardProps['data'][][];
+    items: SkillCardProps['data'][];
   };
 };
 
@@ -18,33 +17,33 @@ type SkillsBlockOrganismProps = SkillsBlockOrganismOwnProps &
   Omit<SecondaryLayoutBlockProps, keyof SkillsBlockOrganismOwnProps>;
 
 const SkillsBlockOrganism = (
-  { className, data, ...props }: SkillsBlockOrganismProps,
+  { className, data, headerProps, ...props }: SkillsBlockOrganismProps,
   ref: SkillsBlockOrganismProps['ref']
 ) => {
   return (
     <SecondaryLayoutBlock
-      className={cn('min-h-fit 2xl:min-h-fit', className)}
+      className={cn(
+        'grid min-h-fit w-9/10 max-w-screen-xl sm:grid-cols-12 2xl:min-h-fit',
+        className
+      )}
       data={{
         title: data.title
+      }}
+      headerProps={{
+        ...headerProps,
+        className: cn('sm:col-span-full', headerProps?.className)
       }}
       ref={ref}
       {...props}
     >
-      <div className='flex w-full flex-col gap-[--gap] [--gap:theme(spacing.xs)]'>
-        {data.items.map((items, i) => (
-          <HorizontalScroll
-            baseVelocity={i % 2 === 0 ? 1 : -1}
-            key={i}
-          >
-            {items.map((data) => (
-              <SkillCard
-                data={data}
-                key={data.id}
-              />
-            ))}
-          </HorizontalScroll>
+      <ul className='m-0 grid gap-xs p-0 [direction:rtl] sm:col-span-7 sm:col-end-13 sm:grid-cols-3 md:grid-cols-4'>
+        {data.items.map((data) => (
+          <SkillCard
+            data={data}
+            key={data.id}
+          />
         ))}
-      </div>
+      </ul>
     </SecondaryLayoutBlock>
   );
 };

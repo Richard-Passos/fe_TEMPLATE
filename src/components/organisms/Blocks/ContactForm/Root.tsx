ca4998f1bde5aa2c791b91caab47c0855807e832
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-23 19:29:42 -0300

    FEAT add ContactFormBlock

---
+++ src/components/organisms/Blocks/ContactForm/Root.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { forwardRef } from 'react';
+
+import { yFullScrollAnim } from '@/animations/scroll';
+import { Lines, ScrollAnimate, Text, Title } from '@/components/atoms';
+import { TextProps } from '@/components/atoms/Text';
+import { TitleProps } from '@/components/atoms/Title';
+import ContactForm, {
+  ContactFormProps
+} from '@/components/organisms/Forms/Contact';
+import { cn } from '@/utils';
+
+import CleanLayoutBlock, { CleanLayoutBlockProps } from '../Layout/Clean';
+
+type ContactFormBlockOrganismOwnProps = {
+  data: Pick<ContactFormProps, 'fields' | 'to' | 'optionalLabel'> & {
+    title: TitleProps['children'];
+    description: TextProps['children'];
+  };
+};
+
+type ContactFormBlockOrganismProps = ContactFormBlockOrganismOwnProps &
+  Omit<CleanLayoutBlockProps, keyof ContactFormBlockOrganismOwnProps>;
+
+const ContactFormBlockOrganism = (
+  { data, className, ...props }: ContactFormBlockOrganismProps,
+  ref: ContactFormBlockOrganismProps['ref']
+) => {
+  return (
+    <CleanLayoutBlock
+      className={cn(
+        'grid w-9/10 max-w-screen-xl gap-xl sm:grid-cols-12',
+        className
+      )}
+      ref={ref}
+      {...props}
+    >
+      <div className='relative size-full overflow-hidden bg-white dark:bg-black sm:col-span-5'>
+        <ScrollAnimate config={yFullScrollAnim}>
+          <Lines className='!opacity-60 [background-size:83.333px_66.666px]' />
+        </ScrollAnimate>
+
+        <span className='pointer-events-none absolute inset-0 rounded-inherit border opacity-60' />
+      </div>
+
+      <div className='sm:col-span-7'>
+        <Title
+          className='sm:max-w-lg'
+          order={2}
+        >
+          {data.title}
+        </Title>
+
+        <Text className='mt-xs text-sm text-dimmed sm:max-w-sm'>
+          {data.description}
+        </Text>
+
+        <ContactForm
+          className='mt-xl'
+          fields={data.fields}
+          optionalLabel={data.optionalLabel}
+          to={data.to}
+        />
+      </div>
+    </CleanLayoutBlock>
+  );
+};
+
+export default forwardRef(ContactFormBlockOrganism);
+export type { ContactFormBlockOrganismProps };
---
+++ src/components/organisms/Blocks/ContactForm/index.ts
@@ -0,0 +1,6 @@
+import ContactFormBlock, {
+  ContactFormBlockOrganismProps as ContactFormBlockProps
+} from './Root';
+
+export default ContactFormBlock;
+export type { ContactFormBlockProps };

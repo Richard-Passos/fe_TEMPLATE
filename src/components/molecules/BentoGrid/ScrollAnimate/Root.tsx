f1aa40807000ad5d5a741f8343c3ac1e637b8fdd
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-26 18:40:59 -0300

    CHORE change scroll animation

--- src/components/molecules/BentoGrid/ScrollAnimate/Root.tsx
+++ src/components/molecules/BentoGrid/ScrollAnimate/Root.tsx
@@ -6,21 +6,14 @@ import {
   ScrollAnimateProps
 } from '@/components/atoms/ScrollAnimate';
 
-const SCROLL_OFFSET = ['0 1', '0 .55'],
+const SCROLL_OFFSET = ['0 1', '0 .6'],
   ANIMATION_CONFIG = {
     y: {
       scrollConfig: {
         offset: SCROLL_OFFSET
       },
       prop: 'y',
-      propPoints: ['50%', '0%']
-    } as ScrollAnimateConfigOptions,
-    rotate: {
-      scrollConfig: {
-        offset: SCROLL_OFFSET
-      },
-      prop: '--tw-rotate',
-      propPoints: ['12deg', '0deg']
+      propPoints: ['25%', '0%']
     } as ScrollAnimateConfigOptions,
     opacity: {
       scrollConfig: {
@@ -34,7 +27,6 @@ const SCROLL_OFFSET = ['0 1', '0 .55'],
 type BentoGridScrollAnimateMoleculeOwnProps = {
   config?: {
     y?: ScrollAnimateConfigOptions;
-    rotate?: ScrollAnimateConfigOptions;
     opacity?: ScrollAnimateConfigOptions;
   };
 };
@@ -53,12 +45,10 @@ const BentoGridScrollAnimateMolecule = (
       {...props}
       config={{ ...ANIMATION_CONFIG.y, ...config?.y }}
     >
-      <ScrollAnimate config={{ ...ANIMATION_CONFIG.rotate, ...config?.rotate }}>
-        <ScrollAnimate
-          config={{ ...ANIMATION_CONFIG.opacity, ...config?.opacity }}
-        >
-          {children}
-        </ScrollAnimate>
+      <ScrollAnimate
+        config={{ ...ANIMATION_CONFIG.opacity, ...config?.opacity }}
+      >
+        {children}
       </ScrollAnimate>
     </ScrollAnimate>
   );

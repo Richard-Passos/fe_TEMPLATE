c33dc8166b3f896e33fdb539f3ca5fd94af610af
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-27 21:17:36 -0300

    FIX pointer events

--- src/components/organisms/Heros/Primary/Root.tsx
+++ src/components/organisms/Heros/Primary/Root.tsx
@@ -71,7 +71,7 @@ const PrimaryHeroOrganism = (
           </div>
         </PrimaryHeroScrollAnimate>
 
-        <span className='absolute inset-0 rounded-inherit border opacity-60' />
+        <span className='pointer-events-none absolute inset-0 rounded-inherit border opacity-60' />
       </div>
 
       <ScrollIndicator className='absolute bottom-[calc(var(--inset)*1.5)] right-[calc(var(--inset)*1.5)] max-sm:hidden' />

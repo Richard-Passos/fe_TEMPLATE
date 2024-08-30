6ebf2aa419127487e0dbc93c3acdb79fa555e035
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-30 14:14:03 -0300

    CHORE add Hover

--- src/components/organisms/Cards/Skill/Root.tsx
+++ src/components/organisms/Cards/Skill/Root.tsx
@@ -6,6 +6,10 @@ import { TitleProps } from '@/components/atoms/Title';
 import Card from '@/components/molecules/Card';
 import { cn } from '@/utils';
 
+import SkillCardHover from './Hover';
+
+const DELAY = 500;
+
 type SkillCardOrganismOwnProps = {
   data: {
     id: string;
@@ -24,35 +28,34 @@ const SkillCardOrganism = (
   ref: SkillCardOrganismProps['ref']
 ) => {
   return (
-    <div
-      className={cn(
-        'group/card aspect-square w-full max-w-40 perspective-1000',
-        className
-      )}
-      ref={ref}
-      {...props}
-    >
-      <div className='relative size-full transition-transform duration-500 ease-backOut transform-style-3d group-hover/card:rotate-y-180'>
-        <Card.Root className='absolute items-center justify-center backface-hidden'>
-          <div className='flex size-1/2 items-center justify-center rounded bg-gray-1 dark:bg-dark-5'>
-            <Icon
-              className='size-1/2'
-              src={data.icon}
-            />
-          </div>
-        </Card.Root>
+    <SkillCardHover delay={DELAY}>
+      <div
+        className={cn('group/card aspect-square perspective-1000', className)}
+        ref={ref}
+        {...props}
+      >
+        <div className='relative size-full transition-transform duration-500 ease-backOut transform-style-3d group-data-[active=true]/card:rotate-y-180'>
+          <Card.Root className='absolute items-center justify-center backface-hidden'>
+            <div className='flex size-1/2 items-center justify-center rounded bg-gray-1 dark:bg-dark-5'>
+              <Icon
+                className='size-1/2'
+                src={data.icon}
+              />
+            </div>
+          </Card.Root>
 
-        <Card.Root className='absolute items-center justify-center rotate-y-180 backface-hidden'>
-          <Title
-            className='w-full break-words text-center'
-            component='h4'
-            order={6}
-          >
-            {data.title}
-          </Title>
-        </Card.Root>
+          <Card.Root className='absolute items-center justify-center rotate-y-180 backface-hidden'>
+            <Title
+              className='w-full break-words text-center'
+              component='h4'
+              order={6}
+            >
+              {data.title}
+            </Title>
+          </Card.Root>
+        </div>
       </div>
-    </div>
+    </SkillCardHover>
   );
 };
 

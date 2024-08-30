b921f3d8bda2d9385c79571f1e2f82b1e0ab39fb
Richard Passos <richard.ac.passos12@gmail.com>
2024-08-30 14:13:46 -0300

    CHORE style

--- src/components/organisms/Footer/Root.tsx
+++ src/components/organisms/Footer/Root.tsx
@@ -52,73 +52,78 @@ const FooterOrganism = (
       }
       {...props}
     >
-      <section className='mb-xl w-9/10 pt-[--section-spacing-md]'>
-        <Title
-          className='font-semibold'
-          component='h3'
-          order={6}
-        >
-          {t('cta.subtitle')}
-        </Title>
+      <div className='relative z-10 flex w-9/10 grow flex-col'>
+        <section className='mb-xl pt-[--section-spacing-md]'>
+          <Title
+            className='max-w-xs font-semibold'
+            component='h3'
+            order={6}
+          >
+            {t('cta.subtitle')}
+          </Title>
 
-        <Title
-          className='break-words leading-none'
-          component='h2'
-          order={3}
-        >
-          {t.rich('cta.title')}
-        </Title>
+          <Title
+            className='max-w-xs break-words uppercase leading-none sm:max-w-sm'
+            component='h2'
+            order={3}
+          >
+            {t.rich('cta.title')}
+          </Title>
 
-        <div className='mt-md flex flex-wrap items-center gap-xs'>
-          <Action href='contact'>{t('cta.action.label')}</Action>
+          <div className='mt-md flex flex-wrap items-center gap-xs'>
+            <Action href='contact'>{t('cta.action.label')}</Action>
 
-          {socialKeys.map((key) => (
-            <Action
-              aria-label={pt(`socials.${key}.label`)}
-              href={pt(`socials.${key}.href`)}
-              isIconOnly
-              key={key}
-              size='input-sm'
-              variant='default'
-            >
-              <Icon
-                className='absolute size-2/3'
-                src={pt(`socials.${key}.icon`)}
-              />
-            </Action>
-          ))}
-        </div>
-      </section>
-
-      <div className='mt-auto flex w-9/10 items-end justify-between'>
-        <section>
-          <Text className='font-semibold'>
-            {t('location', {
-              country: pt('location.country'),
-              state: pt('location.state')
-            })}
-          </Text>
-
-          <Text className='font-semibold'>
-            <LocalTime />
-          </Text>
+            {socialKeys.map((key) => (
+              <Action
+                aria-label={pt(`socials.${key}.label`)}
+                href={pt(`socials.${key}.href`)}
+                isIconOnly
+                key={key}
+                size='input-sm'
+                variant='default'
+              >
+                <Icon
+                  className='absolute size-2/3'
+                  src={pt(`socials.${key}.icon`)}
+                />
+              </Action>
+            ))}
+          </div>
         </section>
 
-        <section className='text-end'>
-          <Text className='text-sm font-medium'>
-            {t.rich('madeBy', {
-              creator: (chunks) => (
-                <Link
-                  className='text-[1em] font-semibold text-current'
-                  href='https://github.com/Richard-Passos'
-                >
-                  {chunks}
-                </Link>
-              )
-            })}
-          </Text>
+        <div className='mt-auto flex gap-lg max-lg:gap-x-sm max-md:flex-col-reverse'>
+          <div className='mt-auto grow overflow-hidden'>
+            <Logo
+              aria-hidden
+              className='h-auto w-full ![--button-bg:transparent] ![--button-hover:transparent] ![--button-padding-x:0px] [&_svg]:h-full'
+              tabIndex={-1}
+              variant='secondary'
+            />
+          </div>
 
-          <Text className='text-sm font-medium'>
+          <section>
+            <Title
+              className='mt-auto w-fit py-sm'
+              component='p'
+              order={6}
+            >
+              {pt('location.country')}
+              &nbsp;
+              {t('locationSeparator')}
+              &nbsp;
+              {pt('location.state')}
+              &nbsp;
+              {t('locationSeparator')}
+              &nbsp;
+              <LocalTime />
+            </Title>
+
+            <Text className='max-w-xs'>{t('description')}</Text>
+          </section>
+        </div>
+
+        <section className='flex justify-center py-sm max-sm:flex-col sm:justify-between'>
+          <Text className='text-sm font-medium max-sm:text-center'>
             {t.rich('copyright', {
               legal: (chunks) => (
                 <Link
@@ -130,19 +135,23 @@ const FooterOrganism = (
               )
             })}
           </Text>
+
+          <Text className='text-center text-sm font-medium sm:text-end'>
+            {t.rich('madeBy', {
+              creator: (chunks) => (
+                <Link
+                  className='text-[1em] font-semibold text-current'
+                  href='https://github.com/Richard-Passos'
+                >
+                  {chunks}
+                </Link>
+              )
+            })}
+          </Text>
         </section>
       </div>
 
-      <div className='w-full overflow-hidden'>
-        <Logo
-          aria-hidden
-          className='h-auto w-full ![--button-bg:transparent] ![--button-hover:transparent] ![--button-padding-x:0px] [&_svg]:h-full'
-          tabIndex={-1}
-          variant='secondary'
-        />
-      </div>
-
-      <div className='absolute inset-[15%] -z-10 w-auto overflow-hidden rounded-lg'>
+      <div className='absolute inset-[15%] overflow-hidden rounded-lg'>
         <ScrollAnimate config={yFullScrollAnim}>
           <Lines className='!opacity-60 [background-size:83.333px_66.666px]' />
         </ScrollAnimate>

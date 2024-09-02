f88c82c2139c0cfacc34e7c63696f3ad94cb095b
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-02 13:33:20 -0300

    CHORE remove index

--- src/components/organisms/Blocks/ProjectsCatalog/Grid/Root.tsx
+++ src/components/organisms/Blocks/ProjectsCatalog/Grid/Root.tsx
@@ -19,19 +19,19 @@ const ProjectsCatalogGridBlockOrganism = <T,>(
   return (
     <CatalogList
       className={cn(
-        'grid max-w-sm gap-xs sm:max-w-3xl sm:grid-cols-2',
+        'grid w-full max-w-sm gap-xs sm:max-w-3xl sm:grid-cols-2',
         className
       )}
       ref={ref}
       {...props}
     >
-      {({ slug, ...data }, i) => (
+      {({ slug, ...data }) => (
         <li
           className='h-fit sm:even:mt-2xl sm:[&:not(:last-child)]:even:-mb-2xl'
           key={slug}
         >
           <GridProjectCard
-            data={{ index: i, ...data }}
+            data={data}
             href={`projects/${slug}`}
           />
         </li>

a20cedd4880634ddcca40ee2d6a5b5bbeeba09d0
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-03 09:15:02 -0300

    FEAT add Project type

---
+++ src/types/Project.ts
@@ -0,0 +1,25 @@
+type Project = {
+  slug: string;
+  isSelected: boolean;
+  href?: string;
+  title: string;
+  roles: { [key: string]: string };
+  year: number;
+  description: string;
+  thumbnail: {
+    src: string;
+    alt: string;
+  };
+  gif?: {
+    src: '/images/alpaca.webp';
+    alt: string;
+  };
+  images?: {
+    [key: string]: {
+      src: '/images/project.jpeg';
+      alt: string;
+    };
+  };
+};
+
+export default Project;

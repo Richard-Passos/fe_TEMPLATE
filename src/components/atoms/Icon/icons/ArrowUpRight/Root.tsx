781be5674d3d94db2e45883cd7a9c935a2cd05d0
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-03 09:19:44 -0300

    FIX rotate

--- src/components/atoms/Icon/icons/ArrowUpRight/Root.tsx
+++ src/components/atoms/Icon/icons/ArrowUpRight/Root.tsx
@@ -22,7 +22,7 @@ const ArrowUpRightIconAtom = (
     >
       <path
         d='M16.707 8.707a1 1 0 0 1-1.414 0L13 6.414V20a1 1 0 0 1-2 0V6.414L8.707 8.707a1 1 0 0 1-1.414-1.414l4-4a1 1 0 0 1 1.416 0l4 4a1 1 0 0 1-.002 1.414Z'
-        rotate={45}
+        transform='rotate(45 12 12)'
       />
     </svg>
   );

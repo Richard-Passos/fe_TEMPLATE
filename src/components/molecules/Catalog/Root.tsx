f8a03759c15b76ed94ca7371c83b8d7a13262915
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-03 14:35:24 -0300

    CHORE remove Box, CHORE add Slot

--- src/components/molecules/Catalog/Root.tsx
+++ src/components/molecules/Catalog/Root.tsx
@@ -1,36 +1,41 @@
-import { createPolymorphicComponent } from '@mantine/core';
-import { forwardRef } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { ComponentPropsWithRef, forwardRef } from 'react';
 
 import CatalogProvider, { CatalogProviderProps } from '@/Providers/Catalog';
-import Box, { BoxProps } from '@/components/atoms/Box';
+import { AsChildProps } from '@/components/atoms/Slot';
 
 type CatalogMoleculeOwnProps = {};
 
-type CatalogMoleculeProps = CatalogMoleculeOwnProps &
-  Omit<BoxProps, keyof CatalogMoleculeOwnProps>;
+type CatalogMoleculeProps = AsChildProps<
+  CatalogMoleculeOwnProps &
+    Omit<ComponentPropsWithRef<'section'>, keyof CatalogMoleculeOwnProps>
+>;
 
 const CatalogMolecule = forwardRef(
-  (props: CatalogMoleculeProps, ref: CatalogMoleculeProps['ref']) => {
+  (
+    { asChild, ...props }: CatalogMoleculeProps,
+    ref: CatalogMoleculeProps['ref']
+  ) => {
+    if (asChild)
+      return (
+        <Slot
+          ref={ref}
+          {...props}
+        />
+      );
+
     return (
-      <Box
-        component='section'
+      <section
         ref={ref}
-        {...props}
+        {...(props as ComponentPropsWithRef<'section'>)}
       />
     );
   }
 );
 CatalogMolecule.displayName = 'CatalogMolecule';
 
-type CatalogMoleculeWithProviderOwnProps = {
-  ref?: CatalogMoleculeProps['ref'];
-};
-
-type CatalogMoleculeWithProviderProps<T> = CatalogMoleculeWithProviderOwnProps &
-  Omit<
-    CatalogProviderProps<T> & CatalogMoleculeProps,
-    keyof CatalogMoleculeWithProviderOwnProps
-  >;
+type CatalogMoleculeWithProviderProps<T> = CatalogProviderProps<T> &
+  CatalogMoleculeProps;
 
 const CatalogMoleculeWithProvider = <T,>(
   { items, url, ...props }: CatalogMoleculeWithProviderProps<T>,
@@ -49,8 +54,5 @@ const CatalogMoleculeWithProvider = <T,>(
   );
 };
 
-export default createPolymorphicComponent<
-  'section',
-  CatalogMoleculeWithProviderProps<any>
->(forwardRef(CatalogMoleculeWithProvider));
+export default forwardRef(CatalogMoleculeWithProvider);
 export type { CatalogMoleculeProps, CatalogMoleculeWithProvider };

e2699e3f30c49336e20f153285c996963f73bec8
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-06 01:45:00 -0300

    REFA, move ScrollTop, CHORE make it a slot

---
+++ src/components/atoms/ScrollTo/index.ts
@@ -0,0 +1,4 @@
+import ScrollTo, { ScrollToAtomProps as ScrollToProps } from './Root';
+
+export default ScrollTo;
+export type { ScrollToProps };
---
+++ src/components/molecules/Form/Watch/index.ts
@@ -0,0 +1,4 @@
+import FormWatch, { FormWatchMoleculeProps as FormWatchProps } from './Root';
+
+export default FormWatch;
+export type { FormWatchProps };

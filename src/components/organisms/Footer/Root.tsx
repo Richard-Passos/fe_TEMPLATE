8524490ad218dd94b901162c020ace8df140c86a
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-06 14:16:47 -0300

    CHORE style

--- src/components/organisms/Footer/Root.tsx
+++ src/components/organisms/Footer/Root.tsx
@@ -98,8 +98,12 @@ const FooterOrganism = (
 
         <div className='mt-auto flex gap-lg max-lg:gap-x-sm max-md:flex-col-reverse'>
           <Logo
-            className='mt-auto h-auto w-full grow ![--button-bg:transparent] ![--button-hover:transparent] ![--button-padding-x:0px] [&_svg]:h-full'
+            className='mt-auto h-auto w-full [&_svg]:size-full'
             variant='secondary'
+            style={{
+              '--button-hover': 'transparent',
+              '--button-padding-x': '0px'
+            }}
           />
 
           <section>

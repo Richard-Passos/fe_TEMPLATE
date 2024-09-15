ce8217e2991c47d442e2b5d67e84d0b16dad21b6
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-15 17:34:56 -0300

    CHORE data

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,15 +1,14 @@
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 
+import rich from '@/rich';
 import { isLocale } from '@/utils';
-
-import rich from '../rich';
 
 export default getRequestConfig(async ({ locale }) => {
   if (!isLocale(locale)) notFound();
 
   return {
-    messages: (await import(`@/data/${locale}.json`)).default,
+    messages: (await import(`@/data/${locale}/root.json`)).default,
     defaultTranslationValues: rich
   };
 });

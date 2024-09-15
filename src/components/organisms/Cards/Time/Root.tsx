e256e104a9bc5a4ba0376cb90f2aeb35450f35b1
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-15 17:58:09 -0300

    CHORE add LocalTime, CHORE data

--- src/components/organisms/Cards/Time/Root.tsx
+++ src/components/organisms/Cards/Time/Root.tsx
@@ -1,13 +1,14 @@
 import { ReactNode, forwardRef } from 'react';
 
-import { Text, Title } from '@/components/atoms';
+import { LocalTime, Text, Title } from '@/components/atoms';
 import Card, { CardRootProps } from '@/components/molecules/Card';
 import { cn } from '@/utils';
+import serialize, { Node } from '@/utils/serialize';
 
 type TimeCardOrganismOwnProps = {
   data: {
-    title: ReactNode;
-    time: ReactNode;
+    title: Node[];
+    time?: ReactNode;
   };
 };
 
@@ -25,15 +26,14 @@ const TimeCardOrganism = (
       {...props}
     >
       <Title
-        className='text-center text-sm uppercase text-dimmed'
-        component='h4'
+        className='relative z-10 text-center text-sm uppercase text-dimmed'
         order={4}
       >
-        {data.title}
+        {serialize(data.title)}
       </Title>
 
       <Text className='text-center text-[14vw] font-bold leading-none sm:text-[min(8vw,6rem)]'>
-        {data.time}
+        {data.time ? data.time : <LocalTime />}
       </Text>
     </Card.Root>
   );

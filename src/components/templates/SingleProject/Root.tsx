47bbfad9125b0f2d97c107caf51c0f607215ed67
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-16 21:15:14 -0300

    CHORE add dynamic  blocks

--- src/components/templates/SingleProject/Root.tsx
+++ src/components/templates/SingleProject/Root.tsx
@@ -1,28 +1,49 @@
-import SingleProjectAdjacents, {
-  SingleProjectAdjacentsProps
-} from './Adjacents';
+import { ComponentType } from 'react';
+
+import { Theme, TypeVariants } from '@/types';
+import { cn } from '@/utils';
+
+import Blocks from './Blocks';
 import SingleProjectHero, { SingleProjectHeroProps } from './Hero';
-import SingleProjectImages, { SingleProjectImagesProps } from './Images';
 
 type SingleProjectTemplateProps = {
   hero: SingleProjectHeroProps;
-  blocks: {
-    images: SingleProjectImagesProps;
-    adjacents: SingleProjectAdjacentsProps;
-  };
+  blocks?: TypeVariants<typeof Blocks>[];
 };
 
 const SingleProjectTemplate = ({
   hero,
   blocks
 }: SingleProjectTemplateProps) => {
+  let lastTheme: Theme = hero.theme;
+
   return (
     <>
       <SingleProjectHero {...hero} />
 
-      <SingleProjectImages {...blocks.images} />
+      {blocks?.map(({ type, id, className, theme, ...props }) => {
+        const Block = Blocks[type] as ComponentType<any>;
 
-      <SingleProjectAdjacents {...blocks.adjacents} />
+        const isSameTheme = lastTheme === theme;
+
+        const Component = (
+          <Block
+            className={cn(
+              'last:pb-[--section-spacing-lg] has-[+[data-has-transition="true"]]:pb-[--section-spacing-lg]',
+              className
+            )}
+            hasTransition={!isSameTheme}
+            id={id}
+            key={id}
+            theme={theme}
+            {...props}
+          />
+        );
+
+        lastTheme = theme;
+
+        return Component;
+      })}
     </>
   );
 };

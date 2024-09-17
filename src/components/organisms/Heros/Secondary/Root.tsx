cbef959c017d46b645d9b3cd79f100f6427d0cd5
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-16 21:16:04 -0300

    CHORE add description and actions, CHORE add serialize

--- src/components/organisms/Heros/Secondary/Root.tsx
+++ src/components/organisms/Heros/Secondary/Root.tsx
@@ -1,13 +1,26 @@
-import { ReactNode, forwardRef } from 'react';
+import { forwardRef } from 'react';
 
 import { yFullScrollAnim } from '@/animations/scroll';
 import { Lines, ScrollAnimate, Title } from '@/components/atoms';
+import Action, { ActionProps } from '@/components/molecules/Action';
 import Section, { SectionProps } from '@/components/organisms/Section';
-import { cn } from '@/utils';
+import { cn, renderComp } from '@/utils';
+import serialize, { Node } from '@/utils/serialize';
+
+type Action = {
+  href?: string;
+  label?: Node[];
+  onClick?: ActionProps['onClick'];
+};
 
 type SecondaryHeroOrganismOwnProps = {
   data: {
-    title: ReactNode;
+    title: Node[];
+    description?: Node[];
+    actions?: {
+      primary?: Action;
+      secondary?: Action;
+    };
   };
 };
 
@@ -15,7 +28,7 @@ type SecondaryHeroOrganismProps = SecondaryHeroOrganismOwnProps &
   Omit<SectionProps, keyof SecondaryHeroOrganismOwnProps>;
 
 const SecondaryHeroOrganism = (
-  { className, data, ...props }: SecondaryHeroOrganismProps,
+  { data, className, ...props }: SecondaryHeroOrganismProps,
   ref: SecondaryHeroOrganismProps['ref']
 ) => {
   return (
@@ -34,8 +47,43 @@ const SecondaryHeroOrganism = (
           className='relative z-10 max-w-md break-words uppercase md:max-w-lg lg:max-w-xl xl:max-w-[75%]'
           order={1}
         >
-          {data.title}
+          {serialize(data.title)}
         </Title>
+
+        {renderComp(
+          <section className='max-w-2xl'>
+            {serialize(data.description)}
+          </section>,
+          [data.description?.length]
+        )}
+
+        {renderComp(
+          <section className='mt-xl flex items-center gap-xs'>
+            {renderComp(
+              <Action
+                as={data.actions?.secondary?.href ? 'link' : 'button'}
+                variant='default'
+                href={data.actions?.secondary?.href ?? ''}
+                onClick={data.actions?.secondary?.onClick}
+              >
+                {serialize(data.actions?.secondary?.label)}
+              </Action>,
+              [data.actions?.secondary]
+            )}
+
+            {renderComp(
+              <Action
+                as={data.actions?.primary?.href ? 'link' : 'button'}
+                href={data.actions?.primary?.href ?? ''}
+                onClick={data.actions?.primary?.onClick}
+              >
+                {serialize(data.actions?.primary?.label)}
+              </Action>,
+              [data.actions?.primary]
+            )}
+          </section>,
+          [data.actions?.primary ?? data.actions?.secondary]
+        )}
 
         <div className='absolute inset-y-0 right-0 flex w-2/3 max-w-md items-center justify-center overflow-hidden rounded-lg bg-white dark:bg-dark-8'>
           <ScrollAnimate config={yFullScrollAnim}>

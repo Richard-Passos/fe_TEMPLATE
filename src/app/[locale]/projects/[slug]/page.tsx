91c7efa30c75765047129f54d1f0c099923a7db2
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-16 21:10:11 -0300

    FEAT add singleProject page

---
+++ src/app/[locale]/projects/[slug]/page.tsx
@@ -0,0 +1,69 @@
+import { Metadata } from 'next';
+import { unstable_setRequestLocale } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+
+import { pagesApi, projectsApi } from '@/api';
+import { SingleProjectTemplate } from '@/components/templates';
+import { entries, normId } from '@/utils';
+
+import { LayoutParams } from '../../layout';
+
+type SingleProjectPageOwnProps = {};
+
+type SingleProjectPageParams = {
+  params: { slug: string } & LayoutParams['params'];
+};
+
+type SingleProjectPageProps = SingleProjectPageOwnProps &
+  SingleProjectPageParams;
+
+const SingleProjectPage = async ({
+  params: { slug, locale }
+}: SingleProjectPageProps) => {
+  unstable_setRequestLocale(locale);
+
+  slug = normId(slug);
+
+  const res = await pagesApi.getSingleProject({ slug, locale });
+
+  if (!res.ok) return notFound();
+
+  const page = res.data;
+
+  return (
+    <SingleProjectTemplate
+      blocks={
+        entries(page.blocks).map(([key, value]) => ({
+          id: normId(key),
+          type: key,
+          ...value
+        })) as any
+      }
+      hero={page.hero}
+    />
+  );
+};
+
+const generateMetadata = async ({
+  params: { slug, locale }
+}: SingleProjectPageParams): Promise<Metadata> => {
+  const res = await pagesApi.getSingleProject({ slug, locale });
+
+  if (!res.ok) return notFound();
+
+  const { metadata } = res.data;
+
+  return metadata;
+};
+
+const generateStaticParams = async ({ params: { locale } }: LayoutParams) => {
+  const res = await projectsApi.get({ locale, isSelected: true });
+
+  const projects = res.ok ? res.data : [];
+
+  return projects.map((d) => ({ slug: d.slug }));
+};
+
+export default SingleProjectPage;
+export { generateMetadata, generateStaticParams };
+export type { SingleProjectPageProps, SingleProjectPageParams };

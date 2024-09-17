1ec9c88a694bf2a3f2ef544e7e1541f3203c4caf
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-16 21:14:03 -0300

    CHORE remove unn code

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,14 +1,12 @@
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 
-import rich from '@/rich';
 import { isLocale } from '@/utils';
 
 export default getRequestConfig(async ({ locale }) => {
   if (!isLocale(locale)) notFound();
 
   return {
-    messages: (await import(`@/data/${locale}/root.json`)).default,
-    defaultTranslationValues: rich
+    messages: {}
   };
 });

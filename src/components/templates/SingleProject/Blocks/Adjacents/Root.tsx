3ec14b6017da97d44aedbb1fa5ed348fc6127a02
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-17 17:16:34 -0300

    CHORE style

--- src/components/templates/SingleProject/Blocks/Adjacents/Root.tsx
+++ src/components/templates/SingleProject/Blocks/Adjacents/Root.tsx
@@ -41,17 +41,23 @@ const SingleProjectAdjacentsTemplate = (
           className='h-fit'
           disabled={!data.prev.href}
           href={data.prev.href}
-          variant='light'
+          magneticProps={{
+            limit: {
+              x: 0.15,
+              y: 0.35
+            }
+          }}
+          variant='default'
         >
           <div className='flex justify-start gap-2 py-sm'>
             <ArrowLeftIcon className='mb-auto size-[1.25em]' />
 
             <div className='flex flex-col items-start gap-0.5'>
-              <span className='max-w-24 overflow-hidden text-ellipsis text-xs opacity-50'>
+              <span className='max-w-24 overflow-hidden text-ellipsis text-xs font-normal tracking-wide opacity-50'>
                 {data.prev.name}
               </span>
 
-              <span>{data.prev.label}</span>
+              <span className='mt-1'>{data.prev.label}</span>
             </div>
           </div>
         </Action>
@@ -61,15 +67,21 @@ const SingleProjectAdjacentsTemplate = (
           className='h-fit'
           disabled={!data.next.href}
           href={data.next.href}
-          variant='light'
+          magneticProps={{
+            limit: {
+              x: 0.15,
+              y: 0.35
+            }
+          }}
+          variant='default'
         >
           <div className='flex justify-end gap-2 py-sm'>
             <div className='flex flex-col items-end gap-0.5'>
-              <span className='max-w-24 overflow-hidden text-ellipsis text-xs opacity-50'>
+              <span className='max-w-24 overflow-hidden text-ellipsis text-xs font-normal tracking-wide opacity-50'>
                 {data.next.name}
               </span>
 
-              <span>{data.next.label}</span>
+              <span className='mt-1'>{data.next.label}</span>
             </div>
 
             <ArrowRightIcon className='mb-auto size-[1.25em]' />

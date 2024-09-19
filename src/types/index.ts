f463cf80f7dbf64c691949fd7eda26d8511fc53c
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-19 15:58:56 -0300

    CHORE export

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,8 +4,10 @@ import ErrorPage from './ErrorPage';
 import Field from './Field';
 import Footer from './Footer';
 import Header from './Header';
+import LegalPage from './LegalPage';
 import Locale from './Locale';
 import Page from './Page';
+import Pages from './Pages';
 import Personal from './Personal';
 import PolymorphicRef from './PolymorphicRef';
 import Project from './Project';
@@ -24,8 +26,10 @@ export type {
   Field,
   Footer,
   Header,
+  LegalPage,
   Locale,
   Page,
+  Pages,
   Personal,
   PolymorphicRef,
   Project,

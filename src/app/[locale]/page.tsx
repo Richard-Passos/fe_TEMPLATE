b33036c1cf33c9c52f6d5f3a94a6ba14bf7dec7a
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-19 16:11:30 -0300

    FIX add Page type

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from 'next/navigation';
 
 import { PageTemplate } from '@/components/templates';
 import { defaultPages } from '@/constants';
+import { Page } from '@/types';
 import { pagesApi } from '@/utils/actions';
 
 import { LayoutParams } from './layout';
@@ -16,7 +17,7 @@ type HomePageProps = HomePageOwnProps & HomePageParams;
 const HomePage = async ({ params: { locale } }: HomePageProps) => {
   unstable_setRequestLocale(locale);
 
-  const res = await pagesApi.getOne({ slug: defaultPages.home, locale });
+  const res = await pagesApi.getOne<Page>({ slug: defaultPages.home, locale });
 
   if (!res.ok) return notFound();
 

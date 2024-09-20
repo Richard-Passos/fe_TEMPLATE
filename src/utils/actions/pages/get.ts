3baae69d994cf70c90756a85b7387442be94a814
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-19 23:31:15 -0300

    CHORE add type

--- src/utils/actions/pages/get.ts
+++ src/utils/actions/pages/get.ts
@@ -6,16 +6,21 @@ import { request } from '@/utils';
 
 type Params = {
   locale: Locale['value'];
+  type?: 'page' | 'error' | 'single-project' | 'legal';
   isSelected?: boolean;
 };
 
 const pagesApiGet = async (
   params: Params,
   config?: Parameters<typeof request>['1']
-) =>
-  await request<PagesResponse>(
-    `/api/pages?locale=${params.locale}&is-selected=${!!params.isSelected}`,
-    config
-  );
+) => {
+  const locale = params.locale ? `locale=${params.locale}` : '',
+    isSelected = params.isSelected ? `is-selected=${!!params.isSelected}` : '',
+    type = params.type ? `type=${params.type}` : '';
+
+  const query = [locale, isSelected, type].join('&');
+
+  return await request<PagesResponse>(`/api/pages?${query}`, config);
+};
 
 export default pagesApiGet;

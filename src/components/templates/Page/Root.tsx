c96d419132fd5ef1225d375fdfb8c6f0611bc01c
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-22 12:32:44 -0300

    CHORE styke

--- src/components/templates/Page/Root.tsx
+++ src/components/templates/Page/Root.tsx
@@ -2,20 +2,15 @@ import { ComponentType } from 'react';
 
 import Blocks from '@/components/organisms/Blocks';
 import { Page } from '@/types';
-import { cn } from '@/utils';
 
 type PageTemplateProps = Pick<Page, 'blocks'>;
 
 const PageTemplate = ({ blocks }: PageTemplateProps) => {
-  return blocks?.map(({ type, id, className, ...props }) => {
+  return blocks?.map(({ type, id, ...props }) => {
     const Block = Blocks[type] as ComponentType<any>;
 
     return (
       <Block
-        className={cn(
-          'last:pb-[--section-spacing-lg] has-[+[data-has-transition="true"]]:pb-[--section-spacing-lg]',
-          className
-        )}
         id={id}
         key={id}
         {...props}

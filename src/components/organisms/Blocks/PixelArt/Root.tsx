c1b628ac279c275ad255d89470e0c4f896d97252
Richard Passos <richard.ac.passos12@gmail.com>
2024-09-26 21:18:43 -0300

    CHORE add Ref Provider

--- src/components/organisms/Blocks/PixelArt/Root.tsx
+++ src/components/organisms/Blocks/PixelArt/Root.tsx
@@ -1,13 +1,11 @@
-'use client';
+import { Suspense, forwardRef } from 'react';
 
-import { Suspense, forwardRef, useRef } from 'react';
-
-import { ColorsProvider } from '@/Providers';
+import { ColorsProvider, RefProvider } from '@/Providers';
 import Section, { SectionProps } from '@/components/organisms/Section';
 import { cn } from '@/utils';
 
 import PixelArtCanvasBlock from './Canvas';
-import PixelArtFormBlock from './Form';
+import PixelArtFormBlock, { PixelArtFormBlockProps } from './Form';
 
 const DEFAULT_SIZE = 4,
   MIN_SIZE = 2,
@@ -28,46 +26,47 @@ const parseSize = (n?: number) =>
     (n ? Math.max(Math.min(n, MAX_SIZE), MIN_SIZE) : defaults.size).toString()
   );
 
-type PixelArtBlockOrganismOwnProps = {};
+type PixelArtBlockOrganismOwnProps = {
+  data: PixelArtFormBlockProps['data'];
+};
 
 type PixelArtBlockOrganismProps = PixelArtBlockOrganismOwnProps &
   Omit<SectionProps, keyof PixelArtBlockOrganismOwnProps>;
 
 const PixelArtBlockOrganism = (
-  { className, ...props }: PixelArtBlockOrganismProps,
+  { data, className, ...props }: PixelArtBlockOrganismProps,
   ref: PixelArtBlockOrganismProps['ref']
 ) => {
-  const canvasRef = useRef<HTMLDivElement>(null);
-
   return (
     <ColorsProvider>
-      <Section
-        className={cn(
-          'w-full items-stretch pb-0 pt-[--header-height] sm:flex-row',
-          className
-        )}
-        ref={ref}
-        {...props}
-      >
-        <Suspense fallback={<>Loading form...</>}>
-          <PixelArtFormBlock
-            canvasRef={canvasRef}
-            defaults={defaults}
-          />
-        </Suspense>
+      <RefProvider>
+        <Section
+          className={cn(
+            'w-full items-stretch pb-0 pt-[--header-height] sm:flex-row',
+            className
+          )}
+          ref={ref}
+          {...props}
+        >
+          <Suspense fallback={<>Loading form...</>}>
+            <PixelArtFormBlock
+              data={data}
+              defaults={defaults}
+            />
+          </Suspense>
 
-        <Suspense fallback={<>Loading canvas...</>}>
-          <PixelArtCanvasBlock
-            defaults={defaults}
-            ref={canvasRef}
-          />
-        </Suspense>
+          <Suspense fallback={<>Loading canvas...</>}>
+            <PixelArtCanvasBlock defaults={defaults} />
+          </Suspense>
 
-        <div
-          className='mx-xs w-1 cursor-col-resize bg-border'
-          id='pixel-art-resize'
-        />
-      </Section>
+          <div
+            className='m-md cursor-col-resize px-2'
+            id='pixel-art-resize'
+          >
+            <div className='h-full w-1 bg-border' />
+          </div>
+        </Section>
+      </RefProvider>
     </ColorsProvider>
   );
 };

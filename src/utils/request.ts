dd04e293418af338e0ef48ceb5943de2a8ab778f
Richard Passos <richard.ac.passos12@gmail.com>
2024-10-02 18:50:15 -0300

    CHORE disabled request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,11 +4,13 @@ const request = async <TResponse>(
   url: string,
   config?: RequestInit
 ): Promise<TResponse> => {
-  url = url.startsWith('/') ? `${baseUrl}${url}` : url;
+  return { ok: false } as TResponse;
+
+  /* url = url.startsWith('/') ? `${baseUrl}${url}` : url;
 
   const response = await fetch(url, { cache: 'no-store', ...config });
 
-  return (await response.json()) as TResponse;
+  return (await response.json()) as TResponse; */
 };
 
 export default request;

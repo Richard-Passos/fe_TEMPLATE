0808e41c1b55d1c120754435c75d3194d46c3d1d
Richard Passos <richard.ac.passos12@gmail.com>
2024-10-02 20:01:09 -0300

    CHORE add hero

--- src/components/templates/Page/Root.tsx
+++ src/components/templates/Page/Root.tsx
@@ -1,22 +1,35 @@
-import { ComponentType } from 'react';
-
-import Blocks from '@/components/organisms/Blocks';
+import { Blocks, Heros } from '@/components/organisms';
 import { Page } from '@/types';
+import { cn } from '@/utils';
 
-type PageTemplateProps = Pick<Page, 'blocks'>;
+type PageTemplateProps = Pick<Page, 'hero' | 'blocks'>;
 
-const PageTemplate = ({ blocks }: PageTemplateProps) => {
-  return blocks?.map(({ type, id, ...props }) => {
-    const Block = Blocks[type] as ComponentType<any>;
+const PageTemplate = ({ hero, blocks }: PageTemplateProps) => {
+  const { type: heroType, ...heroProps } = hero;
 
-    return (
-      <Block
-        id={id}
-        key={id}
-        {...props}
-      />
-    );
-  });
+  const Hero = Heros[heroType];
+
+  return (
+    <>
+      <Hero {...heroProps} />
+
+      {blocks?.map(({ type, id, className, ...props }) => {
+        const Block = Blocks[type];
+
+        return (
+          <Block
+            className={cn(
+              `last:pb-[--section-spacing-lg] has-[+[data-has-transition="true"]]:pb-[--section-spacing-lg]`,
+              className
+            )}
+            id={id}
+            key={id}
+            {...props}
+          />
+        );
+      })}
+    </>
+  );
 };
 export default PageTemplate;
 export type { PageTemplateProps };
